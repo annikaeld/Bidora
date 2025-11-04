96a40ab1c76f53d526d14985bc228b7a7397e299
annikaeld <130365506+annikaeld@users.noreply.github.com>
2025-11-04 10:30:08 +0100

    chore: remove archived React entry src/main.jsx

--- src/main.jsx
+++
@@ -1,5 +0,0 @@
-// This file was an old React entry and has been archived.
-// Project now uses a vanilla JS entry (see /src/vanilla/).
-// If you need to restore React, replace this file with a proper entry.
-
-export default null;
