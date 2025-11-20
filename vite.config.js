ca3ee8bf424582b638232be0c126857628530bf6
annikaeld <130365506+annikaeld@users.noreply.github.com>
2025-11-20 13:58:13 +0100

    deploy problems to netlify

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
-// Static base for repo deployment.
 export default defineConfig({
   base: "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        auctions: resolve(__dirname, "auctions/index.html"),
+        profile: resolve(__dirname, "profile/index.html"),
+        // add any other HTML pages you need
+      },
+    },
+  },
 });
