51875675cd3af206afe248a1f0268c6d00e4820d
annikaeld <130365506+annikaeld@users.noreply.github.com>
2025-11-20 14:27:47 +0100

    #38 issue with files under a folder not beeing deployed

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         auctions: resolve(__dirname, "auctions/index.html"),
+        // Ensure individual pages under `auctions/` are processed too
+        "auctions-edit": resolve(__dirname, "auctions/edit.html"),
+        "auctions-listing": resolve(__dirname, "auctions/listing.html"),
         profile: resolve(__dirname, "profile/index.html"),
         // add any other HTML pages you need
       },
